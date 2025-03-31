f530fcc0116cbb05659243973b745a6e12d673ad
User <mark123ymm@gmail.com>
2025-03-31 14:53:35 -0400

    hyperparameter tuning

--- data.py
+++ data.py
@@ -3,6 +3,20 @@ import numpy as np
 import librosa.display
 import os
 import crepe
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+from torch.optim import lr_scheduler
+import torch.backends.cudnn as cudnn
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import numpy as np
+from PIL import Image
+from tempfile import TemporaryDirectory
 # Generating All Datasets
 # Auto_Tuned_Vocal_is.wav
 # Auto_Tuned_Vocal.wav
@@ -64,3 +78,52 @@ for path, dataset in data:
             plt.plot(time, frequency, label='Pitch (Hz)', color='w')
             plt.savefig(f"{path}_{i}_{k}.png", dpi=300, bbox_inches='tight', pad_inches = 0)
             plt.close()
+
+
+data_transforms = {
+    'training': transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'test': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+data_dir = 'image_data' #'hymenoptera_data'
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x])
+                  for x in ['training', 'test']}
+dataloaders = {x: data.DataLoader(image_datasets[x], batch_size=128,
+                                             shuffle=False, num_workers=8)
+              for x in ['training', 'test']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['training', 'test']}
+class_names = image_datasets['training'].classes
+print(class_names)
+
+pop_mean = []
+pop_std0 = []
+pop_std1 = []
+for i, data in enumerate(dataloaders['training'], 0):
+    # shape (batch_size, 3, height, width)
+    numpy_image = data[0].numpy()
+    
+    # shape (3,)
+    batch_mean = np.mean(numpy_image, axis=(0,2,3))
+    batch_std0 = np.std(numpy_image, axis=(0,2,3))
+    batch_std1 = np.std(numpy_image, axis=(0,2,3), ddof=1)
+    
+    pop_mean.append(batch_mean)
+    pop_std0.append(batch_std0)
+    pop_std1.append(batch_std1)
+
+# shape (num_iterations, 3) -> (mean across 0th axis) -> shape (3,)
+pop_mean = np.array(pop_mean).mean(axis=0)
+pop_std0 = np.array(pop_std0).mean(axis=0)
+pop_std1 = np.array(pop_std1).mean(axis=0)
+print(pop_mean, pop_std0, pop_std1)
