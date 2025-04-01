0daa8ef0e8d9b9f336aec1fa5378896fb6373d41
User <mark123ymm@gmail.com>
2025-03-31 20:22:47 -0400

    adjusting hyper params

--- data.py
+++ data.py
@@ -9,8 +9,6 @@ import torch.optim as optim
 import torch.utils.data as data
 from torch.optim import lr_scheduler
 import torch.backends.cudnn as cudnn
-import torchvision
-from torchvision import datasets, models, transforms
 import matplotlib.pyplot as plt
 import time
 import os
@@ -35,20 +33,20 @@ test_original = []
 for root, dirs, files in os.walk('vocal_data/training'):
     for file in files:
         path = os.path.join(root, file)
-        if file == 'Auto_Tuned_Vocal_is.wav' or file == 'Auto_Tuned_Vocal.wav' or file == 'Auto_Tuned.wav':
+        if file == 'Auto_Tuned.wav':
             training_autotuned.append(path)
-        elif file == 'Original_Vocal_is.wav' or file == 'Original_Vocal.wav' or file == 'Original.wav':
+        elif file == 'Original.wav':
             training_original.append(path)
 for root, dirs, files in os.walk('vocal_data/test'):
     for file in files:
         path = os.path.join(root, file)
-        if file == 'Auto_Tuned_Vocal_is.wav' or file == 'Auto_Tuned_Vocal.wav' or file == 'Auto_Tuned.wav':
+        if file == 'Auto_Tuned.wav':
             test_autotuned.append(path)
-        elif file == 'Original_Vocal_is.wav' or file == 'Original_Vocal.wav' or file == 'Original.wav':
+        elif file == 'Original.wav':
             test_original.append(path)
 
-data = [('image_data/training/autotuned/autotuned', training_autotuned), ('image_data/training/original/original', training_original),
-        ('image_data/test/autotuned/autotuned', test_autotuned), ('image_data/test/original/original', test_original)]
+data = [('image_data2/training/autotuned/autotuned', training_autotuned), ('image_data2/training/original/original', training_original),
+        ('image_data2/test/autotuned/autotuned', test_autotuned), ('image_data2/test/original/original', test_original)]
 
 for path, dataset in data:
     for i in range(len(dataset)):
@@ -78,52 +76,3 @@ for path, dataset in data:
             plt.plot(time, frequency, label='Pitch (Hz)', color='w')
             plt.savefig(f"{path}_{i}_{k}.png", dpi=300, bbox_inches='tight', pad_inches = 0)
             plt.close()
-
-
-data_transforms = {
-    'training': transforms.Compose([
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'test': transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-}
-
-data_dir = 'image_data' #'hymenoptera_data'
-image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
-                                          data_transforms[x])
-                  for x in ['training', 'test']}
-dataloaders = {x: data.DataLoader(image_datasets[x], batch_size=128,
-                                             shuffle=False, num_workers=8)
-              for x in ['training', 'test']}
-dataset_sizes = {x: len(image_datasets[x]) for x in ['training', 'test']}
-class_names = image_datasets['training'].classes
-print(class_names)
-
-pop_mean = []
-pop_std0 = []
-pop_std1 = []
-for i, data in enumerate(dataloaders['training'], 0):
-    # shape (batch_size, 3, height, width)
-    numpy_image = data[0].numpy()
-    
-    # shape (3,)
-    batch_mean = np.mean(numpy_image, axis=(0,2,3))
-    batch_std0 = np.std(numpy_image, axis=(0,2,3))
-    batch_std1 = np.std(numpy_image, axis=(0,2,3), ddof=1)
-    
-    pop_mean.append(batch_mean)
-    pop_std0.append(batch_std0)
-    pop_std1.append(batch_std1)
-
-# shape (num_iterations, 3) -> (mean across 0th axis) -> shape (3,)
-pop_mean = np.array(pop_mean).mean(axis=0)
-pop_std0 = np.array(pop_std0).mean(axis=0)
-pop_std1 = np.array(pop_std1).mean(axis=0)
-print(pop_mean, pop_std0, pop_std1)
